18f87f36fb50363285298ed77c8d4dfffbcca620
Pamella Cardoso <pmjcardoso@hotmail.com>
2017-06-23 11:23:20 -0300

    script de validacao do email funcionando

--- quem/script.js
+++ quem/script.js
@@ -1,33 +1,30 @@
-//
-// window.onload = function(){
-//   var botao = document.querySelector('button');
-//
-//   function validarEmail(){
-//       var entradaEmail = document.getElementsByClassName('msgemail')[0].value;
-//       var padrao = document.getElementsByClassName('msgemail')[0].pattern;
-//       var alertaInvalido = document.getElementsByClassName('emailInvalido')[0];
-//
-//       if(entradaEmail.match(padrao)){
-//           alertaInvalido.innerHTML = '';
-//           setTimeout(function() {
-//             var enviar = document.getElementById("enviar");
-//             function change(){
-//               if (this.innerHTML == "Enviar") {
-//                 this.innerHTML="Enviado";
-//                 var nome = document.getElementById('nome');
-//                 var email = document.getElementById('email');
-//                 nome.value = "Nome";
-//                 email.value = "Email";
-//               }
-//             }
-//             enviar.onclick = change;
-//
-//           }, 300);
-//       }
-//       else {
-//           alertaInvalido.innerHTML = 'E-mail inválido.';
-//       }
-//   }
-//   botao.onclick = validarEmail;
-//
-// }
+
+window.onload = function(){
+
+  var botao = document.querySelector('button');
+
+  function validarEmail(){
+      var entradaEmail = document.getElementsByClassName('msgemail')[0].value;
+      var padrao = document.getElementsByClassName('msgemail')[0].pattern;
+      var alertaInvalido = document.getElementsByClassName('emailInvalido')[0];
+
+      if(entradaEmail.match(padrao)){
+          alertaInvalido.innerHTML = '';
+
+          var enviar = document.getElementById("enviar");
+          if (this.innerHTML == "Enviar") {
+            this.innerHTML="Enviado";
+            var nome = document.getElementById('nome');
+            var email = document.getElementById('email');
+            nome.value = "Nome";
+            email.value = "Email";
+          }
+        
+      }
+      else {
+          alertaInvalido.innerHTML = 'E-mail inválido.';
+      }
+  }
+  botao.onclick = validarEmail;
+
+}
