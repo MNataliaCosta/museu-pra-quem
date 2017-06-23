af3e913bc8f4949b88cbc3805bb90f85c061040e
Pamella Cardoso <pmjcardoso@hotmail.com>
2017-06-23 10:02:15 -0300

    botão enviar

--- quem/script.js
+++ quem/script.js
@@ -1,15 +1,31 @@
 //tentativa erro input email sem @
-function validarEmail(){
-  var formatoEmail = document.getElementById("email").value;
-  if (formatoEmail.checkValidity() == false){
-    alert("O email deve ter formato endereco@dominio.com[.br]");
-  }
-}
+//function validarEmail(){
+  // var formatoEmail = document.getElementById("email").value;
+  // if (formatoEmail.checkValidity() == false){
+    // alert("O email deve ter formato endereco@dominio.com[.br]");
+  // }
+// }
 
 //querySelector('') pega o primeiro elemento indicado que aparece na página
-var botaoEnvio = document.getElementsByClassName("btn");
+// var botaoEnvio = document.getElementsByClassName("btn");
 
-botaoEnvio.onclick = alert('teste');
+// botaoEnvio.onclick = alert('teste');
+//
+
+
+window.onload = function(){
+  var enviar = document.getElementById("enviar");
+  function change(){
+    if (this.innerHTML == "Enviar") {
+      this.innerHTML="Enviado";
+      var nome = document.getElementById('nome');
+      var email = document.getElementById('email');
+      nome.value = "Nome";
+      email.value = "Email";
+    }
+  }
+  enviar.onclick = change;
+}
 
 
 /*
